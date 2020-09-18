236f5729fb858e3bdf269685b32bbbfeb475f65d
Ryan de Fost <ryandefost@hotmail.com>
2020-09-18 12:11:11 +0200

    tweede commit TBA

--- Text_based_adventure.py
+++ Text_based_adventure.py
@@ -1,16 +1,37 @@
-print("Hallo ik ben ERROR en jij?")
+print("Hallo ik ben TEST en jij?")
 naam = (input())
 print("Hallo " + naam)
 
 print("Aan welk getal denk ik van 1 tot 5?")
 antwoord1 = input()
 
-else: print("Dit is geen mogelijk antwoord probeer het opnieuw")
-antwoord1 = input()
-if antwoord1 == "1": print ("Ja inderdaad")
-if antwoord1 == "2": print ("Nee helaas")
-if antwoord1 == "3": print ("Nee helaas")
-if antwoord1 == "4": print ("Nee helaas")
-if antwoord1 == "5": print ("Nee helaas")
+punten = 0
+
+if antwoord1 == "1":
+	print ("Ja inderdaad")
+elif antwoord1 == "2":
+	print ("Nee helaas")
+elif antwoord1 == "3":
+	print ("Nee helaas")
+elif antwoord1 == "4":
+	print ("Nee helaas")
+elif antwoord1 == "5":
+	print ("Nee helaas")
+else:
+	print("Dit is geen correct antwoord... En we gaan door!")
+
+if antwoord1 == "1":
+	print ("Je hebt nu" + (punten + 1) + "punten")
+
+print("Oke nu voor een echte vraag!")
+print ("Wat is de beste kleur?")
+print ("A: Geel")
+antwoord2 = input()
+
+
+
+
+
+
 
 
