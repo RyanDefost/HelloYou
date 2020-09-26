1ec7c89717a17b99cf0861151ad52d4b22cd6c1c
Ryan de Fost <ryandefost@hotmail.com>
2020-09-26 15:22:18 +0200

    Meerdere paden toegevoegt aan vragen

--- Ditbenik.py
+++ Ditbenik.py
@@ -1,65 +1,111 @@
 
+import os
+
+bezig1 = True
+
 #Intro + Eeste vraag.
-print("Hallo en welkom bij Dit-ben-ik!")
-print("\nHeb je zin om een paar vragen te beantwoorden?")
-antwoord1 = input("ja of nee?:")
-if antwoord1 == ("ja"):
-    print("Mooi dan gaan we beginnen!")
-elif antwoord1 == ("nee"):
-    print("Pech, we beginnen al!")
-else:
-    print("dit is geen geldig antwoord (of je hebt hooftletters gebruikt), maar we gaan alsnog door!")
-
+print("HALLO, en welkom! Ik ben Ryan en dit is mijn 'dit ben ik' opdracht.")
+print("\nik heb 3 vragen inclusief deze")
+print("Welke vraag wil je beantwoorden?")
+while (bezig1 == True):
+    antwoord1 = input("2 of 3?: ")
+    if antwoord1 == ("2"):
+        print("Mooi dan gaan we beginnenaan vraag 2!")
+        break
+    elif antwoord1 == ("3"):
+        os.system("cls")
+        print("Mooi dan gaan we beginnen aan vraag 3!")
+        break
+    else:
+        os.system("cls")
+        print("dit is geen geldig antwoord, probeer '2' of '3'")
+        continue
+    
 #Tweede vraag.
-input("\nKlaar om te beginnen?")
-print("\nOke tijd voor de 1ste vraag!")
-print("Welke kleur shirt heb ik aan?")
-print("A: Rood \nB: Grijs \nC: Blauw \nD: Hoe moet ik dit weten?")
-print("\n(let op GEBRUIK hooftletters)")
-antwoord2 = input(":")
+while antwoord1 == "2":
+    input("\nKlaar om te beginnen?\n: ")
+    os.system("cls")
+    break
 
-#Tweede antwoord.
-if antwoord2 == "A":
-    print("Zou best kunnen, Vind ze wel mijn mooiste shirts.")
-elif antwoord2 == "B":
-    print("Zou best kunnen, heb er niet veel van dus warschijnlijk niet.")
-elif antwoord2 == "C":
-    print("Zou best kunnen, wel een mooie kleur.")
-elif antwoord2 == "D":
-    print("ik heb daar eigelijk niet aan gedacht... VOLGENDE VRAAG!")
-else:
-    print("Dit was een simpel antwoord wat begrijp je niet aan A, B, C of D?")
+while antwoord1 == "2":
+    print("-----------------------------------------------------------")
+    print("\nWelke kleur shirt heb ik aan?")
+    print("A: Rood \nB: Grijs \nC: Blauw \nD: Hoe moet ik dit weten?")
+    print("\n(let op GEBRUIK hooftletters)")
+    antwoord2 = input(":")
+    
+#Tweede antwoord. (ROOD = antwoord)
+    if antwoord2 == "A":
+        os.system("cls")
+        break
+    elif antwoord2 == "B":
+        os.system("cls")
+        print("Nee, heb er niet veel van dus kleinste kans.")
+        continue
+    elif antwoord2 == "C":
+        os.system("cls")
+        print("Nee, wel een mooie kleur.")
+        continue
+    elif antwoord2 == "D":
+        os.system("cls")
+        print("Geen idee... het antwoord is rood.")
+        continue
+    else:
+        os.system("cls")
+        print("Dit was een simpel antwoord wat begrijp je niet aan A, B, C of D?")
+        continue
+
 
 #Derde vraag.
-input("\nDenk je klaar te zijn voor de 3de vraag?")
-print("MOOI, denk ik heb niet echt naar je geluisterd.")
-print("\nOke de 3de vraag is: \nWat heb ik in mijn telefoon hoesje zitten?")
-print("A: Bankpas & ID \nB: ID & Speelkaart \nC: Pokémonkaart & bankpas")
-antwoord3 = input(":")
+while antwoord1 == "B":
+    input("\nOke denk je klaar te zijn voor de 3de vraag?\n: ")
+    print("MOOI, denk ik... heb niet echt naar je geluisterd.")
+    break
 
+while antwoord1 == "3":
+    print("-----------------------------------------------------------")
+    print("\nOke de 3de vraag is: \nWat heb ik in mijn telefoon hoesje zitten?")
+    print("A: Bankpas & ID \nB: ID & Speelkaart \nC: Pokémonkaart & bankpas")
+    antwoord3 = input(":")
+    
 #Derde antwoord.
-if antwoord3 == "A":
-    print("Alleen de ID klopt, de bankpas kan soms wel erg handig zijn helaas.")
-elif antwoord3 == "B":
-    print("Ja inderdaad ik heb zelf niet echt een antwoord voor de speelkaart ('t is een ruiten 10)")
-elif antwoord3 == "C":
-    print("Nee en waarom zou ik een pokémonkaart bij me hebben, wat is het 2013?")
-else:
-    print("Dit was een simpel antwoord wat begrijp je niet aan A, B of C?")
-
+    if antwoord3 == "A":
+        os.system("cls")
+        print("Nee alleen de ID klopt, de bankpas kan soms wel erg handig zijn helaas.")
+        continue
+    elif antwoord3 == "B":
+        os.system("cls")
+        print("Ja inderdaad ik heb zelf niet echt een antwoord voor de speelkaart ('t is een ruiten 10)")
+        break
+    elif antwoord3 == "C":
+        os.system("cls")
+        print("Nee en waarom zou ik een pokémonkaart bij me hebben, wat is het 2013?")
+        continue
+    else:
+        os.system("cls")
+        print("Dit was een simpel antwoord wat begrijp je niet aan A, B of C?")
+        continue
+        
 #Slot
-print("\nOke dit was de laatste vraag tijd om te gaan")
-antwoord4 = input("type ENTER voor het slot")
+while antwoord1 == "2" or "3":
+    print("---------------------------------------------------------------------")
+    print("\nOke dit was alles voor vandaag, tijd om te gaan!")
+    antwoord4 = input("type ENTER voor het slot\n: ")
+    
+    if antwoord4 == "ENTER":
+        print("\nWow je tickte het nog uit ook... nice")
+        break
+    elif antwoord4 == " ":
+        print("\nJa je kunt ook gewoon enter in clicken waarom niet...")
+        break
+    elif antwoord4 == "enter":
+        print("\nOke mooi, eigelijk met hooftleters, maar oké")
+        break
+    else:
+        print("\nKom op je had letterlijk 1 ding in moeten type type het dan gewoon!")
+        continue
 
-if antwoord4 == "ENTER":
-    print("\nWow je tickte het nog uit ook... nice")
-elif antwoord4 == " ":
-    print("\nJa je kunt ook gewoon enter in clicken waarom niet...")
-elif antwoord4 == "enter":
-    print("\nWow je tickte het nog uit ook, helaas zonder hooftleters")
-else:
-    print("\nKom op je had letterlijk 1 ding in moeten type type het dan gewoon!")
-print("END")
+print("\nEND")
 
 
 
