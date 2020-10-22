e664e83b2106b530dd76f368485c3a4c0563b62c
Ryan de Fost <ryandefost@hotmail.com>
2020-10-22 10:34:40 +0200

    heb 5 á 6 vragen met def toegevoegt aan het verhaal + indeling aanpassing.

--- Text_based_adventure.py
+++ Text_based_adventure.py
@@ -13,8 +13,19 @@ vraag5 = False
 vraag6 = False
 vraag7 = False
 vraag8 = False
-
-
+vraag9 = False
+vraag10 = False
+vraag11 = False
+vraag12 = False
+vraag13 = False
+vraag14 = False
+vraag15 = False
+vraag16 = False
+vraag17 = False
+vraag18 = False
+vraag19 = False
+vraag20 = False
+vraag21 = False
 #-------------------------------------------------------------------------------------------
 #Introductie:
 print("\n--------------------------------------------------------------------------------")
@@ -36,6 +47,155 @@ def def1(antwoord):
                 return "C"
 
 
+#defenitie vraag2
+def def2(antwoord):
+        if antwoord.lower() == "A":
+                print("A2 werkt")
+                return "A"
+        elif antwoord.lower() == "B":
+                print("B2 werkt")
+                return "A"
+        else:
+                print("fout2 werkt")
+                return "C"
+
+
+#defenitie vraag3
+def def3(antwoord):
+        if antwoord.lower() == "nederland":
+                print("A3 werkt")
+                return "A"
+        elif antwoord.lower() == "engeland":
+                print("B3 werkt")
+                return "A"
+        else:
+                print("fout3 werkt")
+                return "C"
+
+
+#defenitie vraag4
+def def4(antwoord):
+        if antwoord.lower() == "a":
+                print("A4 werkt")
+                return "A"
+        elif antwoord.lower() == "b":
+                print("B4 werkt")
+                return "B"
+        else:
+                print("fout4 werkt")
+                return "C"
+
+
+#defenitie vraag5
+def def5(antwoord):
+        if antwoord.lower() == "a":
+                print("A5 werkt")
+                return "A"
+        elif antwoord.lower() == "b":
+                print("B5 werkt")
+                return "B"
+        else:
+                print("fout5 werkt")
+                return "C"
+
+
+#defenitie vraag5i5
+def def5i5(antwoord):
+        if antwoord.lower() == "a":
+                print("A5i5 werkt")
+                return "A"
+        elif antwoord.lower() == "b":
+                print("B5i5 werkt")
+                return "B"
+        else:
+                print("fout5i5 werkt")
+                return "C"
+
+
+#defenitie vraag6
+def def6(antwoord):
+        if antwoord.lower() == "a":
+                print("A6 werkt")
+                return "A"
+        elif antwoord.lower() == "b":
+                print("B6 werkt")
+                return "B"
+        else:
+                print("fout6 werkt")
+                return "C"
+
+
+#defenitie vraag7
+def def7(antwoord):
+        if antwoord.lower() == "a":
+                print("A7 werkt")
+                return "A"
+        elif antwoord.lower() == "b":
+                print("B7 werkt")
+                return "B"
+        else:
+                print("fout7 werkt")
+                return "C"
+
+
+#defenitie vraag8
+def def8(antwoord):
+        if antwoord.lower() == "a":
+                print("A8 werkt")
+                return "A"
+        elif antwoord.lower() == "b":
+                print("B8 werkt")
+                return "B"
+        else:
+                print("fout8 werkt")
+                return "C"
+
+
+#defenitie vraag9
+#def def9(antwoord):
+        #Er is geen Def
+
+
+#defenitie vraag10
+#def def10(antwoord):
+        #Er is geen Def
+
+
+
+#defenitie vraag11
+def def11(antwoord):
+        if antwoord.lower() == "a":
+                print("A11 werkt")
+                return "A"
+        elif antwoord.lower() == "b":
+                print("B11 werkt")
+                return "B"
+        elif antwoord.lower() == "c":
+                print("C11 werkt")
+                return "C"
+        else:
+                print("fout11 werkt")
+                return "D"
+
+
+#defenitie vraag12
+def def12(antwoord):
+        if antwoord.lower() == "a":
+                print("A12 werkt")
+                return "A"
+        elif antwoord.lower() == "b":
+                print("B12 werkt")
+                return "B"
+        elif antwoord.lower() == "c":
+                print("C12 werkt")
+                return "C"
+        else:
+                print("fout12 werkt")
+                return "D"
+
+
+
+#-------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------
 #Vraag 1 / input:
 while vraag1 == True:
@@ -53,39 +213,12 @@ while vraag1 == True:
         else:
                 continue
 
-
-#-------------------------------------------------------------------------------------------
-#defenitie vraag2
-def def2(antwoord):
-        if antwoord.lower() == "A":
-                print("A2 werkt")
-                return "A"
-        elif antwoord.lower() == "B":
-                print("B2 werkt")
-                return "A"
-        else:
-                print("fout2 werkt")
-                return "C"
-
-        
+       
 #-------------------------------------------------------------------------------------------
 #Vraag 2 / input:
 while vraag2 == True:
         print("vraag2 werkt")
         break
-
-#-------------------------------------------------------------------------------------------
-#defenitie vraag3
-def def3(antwoord):
-        if antwoord.lower() == "nederland":
-                print("A3 werkt")
-                return "A"
-        elif antwoord.lower() == "engeland":
-                print("B3 werkt")
-                return "A"
-        else:
-                print("fout3 werkt")
-                return "C"
 
        
 #-------------------------------------------------------------------------------------------
@@ -104,20 +237,6 @@ while vraag3 == True:
 
 
 #-------------------------------------------------------------------------------------------
-#defenitie vraag4
-def def4(antwoord):
-        if antwoord.lower() == "a":
-                print("A4 werkt")
-                return "A"
-        elif antwoord.lower() == "b":
-                print("B4 werkt")
-                return "B"
-        else:
-                print("fout4 werkt")
-                return "C"
-
-
-#-------------------------------------------------------------------------------------------
 #Vraag 4 /input:
 while vraag4 == True:
 
@@ -136,113 +255,43 @@ while vraag4 == True:
 
 
 #-------------------------------------------------------------------------------------------
-#defenitie vraag4
-def def4(antwoord):
-        if antwoord.lower() == "a":
-                print("A4 werkt")
-                return "A"
-        elif antwoord.lower() == "b":
-                print("B4 werkt")
-                return "B"
-        else:
-                print("fout4 werkt")
-                return "C"
-
-
-#-------------------------------------------------------------------------------------------
-#Vraag 4 /input:
-while vraag4 == True:
+#Vraag 6 /input:
+while vraag6 == True:
 
         os.system("cls")
-        print("vraag4 werkt")
-        antwoord4 = input ("Waneer ga je vertrekken\nA: onmiddelijk\nB: eerst voorbereiden\n: ")
+        print("vraag6 werkt")
+        antwoord6 = input ("Je gaat voorbereiden wat pak je eerst?\nA:paspoort pakken\nB:afscheid vriend\n:")
 
-        if def4(antwoord4) == "A":
-                vraag5 = True
+        if def6(antwoord6) == "A":
+                vraag9 = True
                 break
-        elif def4(antwoord4) == "B":
-                vraag6 = True
+        elif def6(antwoord6) == "B":
+                vraag10 = True
                 break
         else:
                 continue
 
 
 #-------------------------------------------------------------------------------------------
-#defenitie vraag4
-def def4(antwoord):
-        if antwoord.lower() == "a":
-                print("A4 werkt")
-                return "A"
-        elif antwoord.lower() == "b":
-                print("B4 werkt")
-                return "B"
-        else:
-                print("fout4 werkt")
-                return "C"
-
-
-#-------------------------------------------------------------------------------------------
-#Vraag 4 /input:
-while vraag4 == True:
+#Vraag 9 /input:
+while vraag9 == True:
 
         os.system("cls")
-        print("vraag4 werkt")
-        antwoord4 = input ("Waneer ga je vertrekken\nA: onmiddelijk\nB: eerst voorbereiden\n: ")
-
-        if def4(antwoord4) == "A":
-                vraag5 = True
-                break
-        elif def4(antwoord4) == "B":
-                vraag6 = True
-                break
-        else:
-                continue
+        print("vraag9 werkt")
+        print("Je pakt je paspoort en gaat snel weg\n")
+        vraag5i5 = True
+        break
 
 
 #-------------------------------------------------------------------------------------------
-#defenitie vraag4
-def def4(antwoord):
-        if antwoord.lower() == "a":
-                print("A4 werkt")
-                return "A"
-        elif antwoord.lower() == "b":
-                print("B4 werkt")
-                return "B"
-        else:
-                print("fout4 werkt")
-                return "C"
-
-
-#-------------------------------------------------------------------------------------------
-#Vraag 4 /input:
-while vraag4 == True:
+#Vraag 10 /input:
+while vraag10 == True:
 
         os.system("cls")
-        print("vraag4 werkt")
-        antwoord4 = input ("Waneer ga je vertrekken\nA: onmiddelijk\nB: eerst voorbereiden\n: ")
-
-        if def4(antwoord4) == "A":
-                vraag5 = True
-                break
-        elif def4(antwoord4) == "B":
-                vraag6 = True
-                break
-        else:
-                continue
-
-
-#-------------------------------------------------------------------------------------------
-#defenitie vraag5
-def def5(antwoord):
-        if antwoord.lower() == "a":
-                print("A5 werkt")
-                return "A"
-        elif antwoord.lower() == "b":
-                print("B5 werkt")
-                return "B"
-        else:
-                print("fout5 werkt")
-                return "C"
+        print("vraag10 werkt")
+        print("Je gaat naar je vriend +Tip voor haven")
+        vraag5i5 = True
+        break
 
 
 #-------------------------------------------------------------------------------------------
@@ -261,6 +310,114 @@ while vraag5 == True:
                 break
         else:
                 continue
+
+
+#-------------------------------------------------------------------------------------------
+#Vraag 7 /input:
+while vraag7 == True:
+
+        os.system("cls")
+        print("vraag7 werkt")
+        antwoord7 = input ("Je besluit de bus te nemen waar ga je heen?\nA:De haven\nB:De grens\n:")
+
+        if def7(antwoord7) == "A":
+                vraag11 = True
+                break
+        elif def7(antwoord7) == "B":
+                vraag12 = True
+                break
+        else:
+                continue
+
+
+#-------------------------------------------------------------------------------------------
+#Vraag 8 /input:
+while vraag8 == True:
+
+        os.system("cls")
+        print("vraag8 werkt")
+        antwoord8 = input ("Je ziet iemand grafitie maken, maar er komt politie wat doe je?\nA:Waarschuw|nB:Niks\n:")
+
+        if def8(antwoord8) == "A":
+                vraag13 = True
+                break
+        elif def8(antwoord8) == "B":
+                vraag14 = True
+                break
+        else:
+                continue
+
+
+#-------------------------------------------------------------------------------------------
+#Vraag 11 /input:
+while vraag11 == True:
+
+        os.system("cls")
+        print("vraag11 werkt")
+        print("je komt aan op de haven\nA:Stiekem mee boot\nB: verder naar grens")
+        if vraag10 == True:
+                print("C:Naaar man van de Tip 'vraag10'")
+        antwoord11 = input 
+
+        if def11(antwoord11) == "A":
+                vraag15 = True
+                break
+        elif def11(antwoord11) == "B":
+                vraag12 = True
+                break
+        elif def11(antwoord11) == "C":
+                vraag16 = True
+                break
+        else:
+                continue
+
+
+#Vraag 12 /input:
+while vraag12 == True:
+
+        os.system("cls")
+        print("vraag12 werkt")
+        print("Wat ga je doen bij de grens")
+        if vraag9 == True:
+                print("A:probeer over de grens te gaan.")
+        else:
+                print("B:Je hebt geen paspoort stiekem langs")
+        print("C:Verder naar de haven")
+        antwoord12 = input(":")
+
+        if def12(antwoord12) == "A":
+                vraag18 = True
+                break
+        elif def12(antwoord12) == "B":
+                vraag19 = True
+                break
+        elif def12(antwoord12) == "C":
+                vraag12b = True
+                break
+        else:
+                continue
+
+
+#Vraag 12b /input:
+while vraag12b == True:
+
+        os.system("cls")
+        print("vraag12b werkt")
+        print("je komt aan op de haven\nA:Stiekem mee met vracht schip")
+        if vraag10 == True:
+                print("B:Naar man van de Tip 'vraag10'")
+        antwoord11 = input
+
+        if def12b(antwoord12b) == "A":
+                vraag15 = True
+                break
+        elif def12b(antwoord12b) == "B":
+                vraag16 = True
+                break
+        else:
+                continue
+
+
 
 print("........................")
         
